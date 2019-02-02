3d8ba6d42daeb3509faca194cd5e2f75240f32cd
VincentiusPramudito <vpramudito@yahoo.com>
2019-02-02 22:12:27 +0700

    add tugas balik kata

---
+++ balikKata.js
@@ -0,0 +1,19 @@
+//Balik Kata//
+
+function balikKata(kata) {
+  var isi = kata;
+  var i = isi.length;
+  var hasil = '';
+  for (i = isi.length; i >= 0; i--){
+    hasil = hasil + isi[i];
+  }
+  return hasil;
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
+=============================================================================
