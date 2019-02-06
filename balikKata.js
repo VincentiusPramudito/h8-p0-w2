5a97189d9524449265a5808785abd5bdf092d719
VincentiusPramudito <vpramudito@yahoo.com>
2019-02-06 11:18:05 +0700

    update jawaban

--- balikKata.js
+++ balikKata.js
@@ -1,10 +1,9 @@
 //Balik Kata//
-
 function balikKata(kata) {
   var isi = kata;
   var i = isi.length;
   var hasil = '';
-  for (i = isi.length; i >= 0; i--){
+  for (i = isi.length-1; i >= 0; i--){
     hasil = hasil + isi[i];
   }
   return hasil;
@@ -16,4 +15,4 @@ console.log(balikKata('John Doe')); // eoD nhoJ
 console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
 console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
 console.log(balikKata('Super')); // repuS
-=============================================================================
+// =============================================================================
